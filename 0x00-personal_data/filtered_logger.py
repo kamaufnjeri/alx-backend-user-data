d8ba68ef1178a95b4dd3501a45d08d4a44789efa
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-09 03:08:41 +0300

    personal data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module for handling sensitive data
+Module for handling Personal Data
 """
 from typing import List
 import re
@@ -9,37 +9,37 @@ from os import environ
 import mysql.connector
 
 
-SENSITIVE_FIELDS = ("name", "email", "phone", "ssn", "password")
+PERSONAL_INFO_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def obfuscate_data(fields: List[str], redaction: str,
                    message: str, separator: str) -> str:
-    """ Obfuscates sensitive data in a log message """
+    """ Returns a log message with sensitive data obfuscated """
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
 
 
-def get_secure_logger() -> logging.Logger:
-    """ Returns a secure logger instance """
+def get_logger() -> logging.Logger:
+    """ Returns a Logger Object """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(SecureFormatter(list(SENSITIVE_FIELDS)))
+    stream_handler.setFormatter(RedactingFormatter(list(PERSONAL_INFO_FIELDS)))
     logger.addHandler(stream_handler)
 
     return logger
 
 
-def get_secure_db_connector() -> mysql.connector.connection.MySQLConnection:
-    """ Returns a secure connector to a MySQL database """
-    username = environ.get("SENSITIVE_DATA_DB_USERNAME", "root")
-    password = environ.get("SENSITIVE_DATA_DB_PASSWORD", "")
-    host = environ.get("SENSITIVE_DATA_DB_HOST", "localhost")
-    db_name = environ.get("SENSITIVE_DATA_DB_NAME")
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to a MySQL database """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
 
     cnx = mysql.connector.connection.MySQLConnection(user=username,
                                                      password=password,
@@ -50,14 +50,15 @@ def get_secure_db_connector() -> mysql.connector.connection.MySQLConnection:
 
 def main():
     """
-    Retrieves sensitive data from a database and logs it securely
+    Obtain a database connection using get_db and retrieves all rows
+    in the users table and display each row under a filtered format
     """
-    db = get_secure_db_connector()
+    db = get_db()
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
     field_names = [i[0] for i in cursor.description]
 
-    logger = get_secure_logger()
+    logger = get_logger()
 
     for row in cursor:
         str_row = ''.join(f'{f}={str(r)}; ' for r, f in zip(row, field_names))
@@ -67,23 +68,22 @@ def main():
     db.close()
 
 
-class SecureFormatter(logging.Formatter):
-    """ Formatter class for secure logging """
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        super(SecureFormatter, self).__init__(self.FORMAT)
+        super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """ Filters values in incoming log records using obfuscate_data """
         record.msg = obfuscate_data(
-            self.fields, self.REDACTION,
-            record.getMessage(), self.SEPARATOR
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR
         )
-        return super(SecureFormatter, self).format(record)
+        return super(RedactingFormatter, self).format(record)
 
 
 if __name__ == '__main__':
