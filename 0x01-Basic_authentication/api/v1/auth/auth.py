0076b28be9cb0a39a63d3afd2a38d887f5c1fba0
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-13 00:58:24 +0300

    require auth function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,22 @@ class Auth:
     """Class to manage api authentications"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """require auth"""
-        return False
+        """require auth paths"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        slashless_exclude_paths: List[str] = []
+
+        for new_path in excluded_paths:
+            slashless_exclude_paths.append(new_path[:-1])
+
+        if path in slashless_exclude_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
