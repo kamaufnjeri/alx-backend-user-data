1c61c9049bd98269907373393e36417714910dac
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-13 01:46:58 +0300

    containing Basic in header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 """A class that inherits from Auth class"""
-from auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """Inherits from Auth"""
-    pass
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str
+    ) -> str:
+        """hat returns the Base64 part of
+        the Authorization header for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+
+        if not type(authorization_header) == str:
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[6:]
