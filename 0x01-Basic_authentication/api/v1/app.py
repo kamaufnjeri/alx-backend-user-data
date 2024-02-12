6608428b775b412adf124c3b577a91735dcbbbd4
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-13 01:34:56 +0300

    created empty class inheriting from Auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,6 +21,11 @@ if AUTH_TYPE == "auth":
 
     auth = Auth()
 
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+
+    auth = BasicAuth()
+
 
 @app.before_request
 def before_request() -> str:
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A class that inherits from Auth class"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """Inherits from Auth"""
+    pass
