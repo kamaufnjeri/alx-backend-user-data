e3d62184ba64a512f6c5a4127c4c4336c6bf2b67
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-13 02:02:00 +0300

    base64 decoding header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """A class that inherits from Auth class"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,23 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str
+    ) -> str:
+        """Return decoded data of base64"""
+
+        if base64_authorization_header is None:
+            return None
+
+        if not type(base64_authorization_header) == str:
+            return None
+
+        try:
+            decoded64 = b64decode(base64_authorization_header)
+            decoded = decoded64.decode('utf-8')
+        except Exception:
+            return None
+
+        return decoded
