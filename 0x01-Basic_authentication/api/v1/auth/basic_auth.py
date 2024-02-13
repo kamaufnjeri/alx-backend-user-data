e37a0c95547a6e0b8bfb54ade91ad588235b063c
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-13 15:17:16 +0300

    take password with :

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -61,7 +61,7 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return (None, None)
 
-        list_items = decoded_base64_authorization_header.split(":")
+        list_items = decoded_base64_authorization_header.split(":", 1)
 
         return (list_items[0], list_items[1])
 
