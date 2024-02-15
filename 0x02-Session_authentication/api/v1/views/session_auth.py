5e16895d919c17c8ba3c96a48b844c19baaeea52
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-15 17:52:53 +0300

    session authentication to login user

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin env python3
+"""new view for session_auth login"""
+from api.v1.views import app_views
+from flask import request, jsonify
+from models.user import User
+import os
+
+
+@app_views.route("/auth_session/login", methods=['POST'], strict_slashes=False)
+def auth_session_login():
+    """post method to retrieve email and password to login user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not email or email == "":
+        return jsonify({"error": "email missing"}), 400
+
+    if not password or password == "":
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        users = User.search({"email": email})
+
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in users:
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+
+            SESSION_NAME = os.getenv("SESSION_NAME")
+            resp = jsonify(user.to_json())
+
+            resp.set_cookie(SESSION_NAME, session_id)
+
+            return resp
+
+    return jsonify({"error": "wrong password"}), 401
--- 0x02-Session_authentication/python.py
+++ 0x02-Session_authentication/python.py
@@ -1,10 +1,3 @@
 import os
 
-# Get the value of a specific environmental variable
-auth_type = os.getenv("AUTH_TYPE")
-
-# Check if the variable is set
-if auth_type:
-    print("AUTH_TYPE is set to:", auth_type)
-else:
-    print("AUTH_TYPE is not set.")
+# Print all environmental variables
