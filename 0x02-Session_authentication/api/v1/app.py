b3727f81a4239b1c2134e2cc61284f3fdf51483f
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-15 16:44:43 +0300

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -49,7 +49,7 @@ def before_request() -> str:
         return
 
     if auth.authorization_header(request) is None\
-        and auth.session_cookie(request) is None:
+            and auth.session_cookie(request) is None:
         abort(401)
 
     current_user = auth.current_user(request)
