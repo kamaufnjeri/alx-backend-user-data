b623e4d66e240f81fa8980a1698cd72804b9694b
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-15 21:19:32 +0300

    session authentication to save to file database

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,6 +36,11 @@ elif AUTH_TYPE == "session_exp_auth":
 
     auth = SessionExpAuth()
 
+elif AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+
+    auth = SessionDBAuth()
+
 
 @app.before_request
 def before_request() -> str:
