b53a62068c44020d172b3c4bd8fafac6f80de75f
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-22 20:31:43 +0300

    user authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,7 @@
 """Flask app for user authentication and session services
 """
 from flask import Flask, jsonify, request, abort, redirect
-
 from auth import Auth
-
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -15,4 +13,3 @@ def welcom() -> str:
     """GET method welcome page
     """
     return jsonify({"message": "Bienvenue"})
-
